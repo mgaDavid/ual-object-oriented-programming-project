a577893d9d61ae554e1ebd16161e98979a050c01
bmteodoro <72469300+bmteodoro@users.noreply.github.com>
2020-11-07 15:16:30 +0000

    JUnit Tests

---
+++ lab_2/src/test/AccountTest.java
@@ -0,0 +1,46 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void getNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDependents() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOverdraft() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOperations() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setOverdraft() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registerOperation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addDependent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeDependent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getClient() {
+    }
+}
---
+++ lab_2/src/test/ClientTest.java
@@ -0,0 +1,70 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @org.junit.jupiter.api.Test
+    void getDocument() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAccounts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAddress() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setContact() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMyAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBirthday() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAddress() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getContact() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeAccount() {
+    }
+}
