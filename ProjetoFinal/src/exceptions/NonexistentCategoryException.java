903df1d92c7dcfeac4602aa991b0bf5dc84ec3a7
Diego Soares <diego@tradingeconomics.com>
2021-01-10 14:55:47 +0000

    First big commit

---
+++ ProjetoFinal/src/exceptions/NonexistentCategoryException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+
+public class NonexistentCategoryException extends Exception {
+
+    public NonexistentCategoryException() {
+        super("Categoria inexistente.");
+    }
+
+    public NonexistentCategoryException(String message) {
+        super(message);
+    }
+}
---
+++ ProjetoFinal/src/exceptions/NonexistentPermissionException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+
+public class NonexistentPermissionException extends Exception {
+
+    public NonexistentPermissionException() {
+        super("Permissão inexistente.");
+    }
+
+    public NonexistentPermissionException(String message) {
+        super(message);
+    }
+}
